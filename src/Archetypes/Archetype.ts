c66e942029a680237073ad43eba57a2ff71c9ca2
Rafael <rafaelratier@hotmail.com>
2022-07-18 22:07:29 -0300

    feat(archetype): adds archetype abstract class

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,27 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetype {
+  private readonly _cost: number = 0;
+  private readonly _special: number = 0;
+  constructor(private readonly _name: string) {}
+
+  get cost(): number {
+    return this._cost;
+  }
+
+  get special(): number {
+    return this._special;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  static createArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
+
+export default Archetype;
